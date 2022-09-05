13d58cd3fe6351e64af5aeb0f1a9396fa22bc97b
Judielle Villa <judiellezinke@yahoo.de>
2022-09-05 10:01:55 +0200

    imported newly created components to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import MyFavorites from './components/MyFavorites/MyFavorites';
+import Random from './components/Random/Random';
+import SearchResults from './components/SearchResults/SearchResults';
+import Trending from './components/Trending/Trending';
 
 function App() {
   return (
     <div className='wrapper'>
       <h1>Welcome</h1>
+      <Trending />
       </div>
   );
 }
