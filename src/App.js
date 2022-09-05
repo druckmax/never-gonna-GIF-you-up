51383910e55e2cb1e6abb7906ea5cc1b77d15906
OVDCI <oraziovalerio.ventura@dci-student.de>
2022-09-05 15:55:38 +0200

    switch done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, {createContext,useState} from 'react';
+import MySwitch from './components/mySwitch';
+
 import './App.scss';
 
+
+export const ThemeContext= createContext(null);
+ 
+
 function App() {
+  const [theme, setTheme]= useState("dark");
+
+  const toggleTheme= () => setTheme((curr)=>(curr==="light" ? "dark":"light"));
+  ;
   return (
-    <div className='wrapper'>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+    <div className='wrapper' id={theme}>
       <h1>Welcome</h1>
+      <div className= 'switch'>
+
+        
+        
+         {<MySwitch toggleTheme={toggleTheme} checked={theme === "dark"}/> }
+      
+        </div>
       </div>
+      </ThemeContext.Provider>
   );
 }
 
