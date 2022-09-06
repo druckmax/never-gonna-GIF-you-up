465b51389dee8ca48a663b2f5fef41cd4dbefbf3
Judielle Villa <judiellezinke@yahoo.de>
2022-09-06 13:34:25 +0200

    added working API fetches for Random, Trending and Search. Also transferred input state to context for SearchResult to get access to the state for url interpolation

--- src/context/context.js
+++ src/context/context.js
@@ -10,8 +10,18 @@ export default function ContextProvider(props) {
     //? Theme Toggle Function
     const toggleTheme= () => setTheme((curr)=>(curr==="light" ? "dark":"light"));
 
+    //? Fetched Data State Storage
+    const [dataFavorites, setDataFavorites] = useState([])
+
+    const [dataSearch, setDataSearch] = useState([])
+    const [input, setInput] = useState("");
+
+    const [dataTrending, setDataTrending] = useState([])
+
+    const [dataRandom, setDataRandom] = useState({})
+
     return (
-        <MainContext.Provider value={{theme, setTheme, toggleTheme}}>
+        <MainContext.Provider value={{theme, setTheme, toggleTheme, dataFavorites, setDataFavorites, dataSearch, setDataSearch, setDataTrending, dataTrending, dataRandom, setDataRandom, input, setInput}}>
             {props.children}
         </MainContext.Provider>
         )
