d4d4cf8d21d2f1331194b3db06cf88c84c941dfa
druckmax <max_sommerfeld@web.de>
2022-09-11 16:26:00 +0200

    Finishes MemeGenerator - adds draggable and resizeable input fields   - adds onClick feature for devices with no hover - fixes notification width overflow on small screens - fixes navbar overflow on touch devices

--- src/components/memeGenerator/MemeGenerator.js
+++ src/components/memeGenerator/MemeGenerator.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import { MainContext } from "../../context/context";
 import { useContext } from "react";
 import { useEffect } from "react";
-import fry_placeholder from './img/take-my-money.gif'
+import fry_placeholder from "./img/take-my-money.gif";
+import Draggable from "react-draggable";
+import {BsTextareaResize} from 'react-icons/bs'
 
 import "./MemeGenerator.scss";
 
@@ -15,18 +17,35 @@ export default function MemeGenerator() {
     // todo Placeholder image or container
     randomImage: fry_placeholder,
   });
+  // Set meme template array
+  const [memeArr, setMemeArr] = useState([]);
+  // Show/Hide memeText resize grabber and border
+  const [showMemeGrabber, setShowMemeGrabber] = useState(false)
+
+  //Fetch meme tempalte array
+  const fetchMemeTemps = async () => {
+    const response = await fetch("https://api.imgflip.com/get_memes");
+    const data = await response.json();
+    setMemeArr(data);
+  };
+
   useEffect(() => {
-    context.fetchRandom();
-  }, [meme]);
+    fetchMemeTemps();
+  }, []);
+
+  // Get random number based on memeArr length
+  const getRandomNumber = (length) =>
+    Math.floor(Math.random() * length);
+
   // creating function to update the objects property "randomImag" which is hold into state "meme". We get a random image everytime this function gets called. The function ges called, when the button gets clicked.
   function getMemeImage() {
-    const memesArray = context.dataRandom?.images?.original;
-    console.log("yes");
-    const url = memesArray.url;
-    setMeme((prevMeme) => ({
-      ...prevMeme,
+    const randomNumber = getRandomNumber(memeArr?.data?.memes.length);
+    const url = memeArr.data.memes[randomNumber].url;
+    setMeme({
+      topText: '',
+      bottomText: '',
       randomImage: url,
-    }));
+    });
   }
   // we apply a handleChange function on the input fields property "Onchange". This function is called everytime the user types something. The function will update our object "meme" in the state.
   function handleChange(e) {
@@ -38,10 +57,15 @@ export default function MemeGenerator() {
       };
     });
   }
-  // we return JSX code. The Input values are given by targeting our state object "meme" --> meme.topText will give us the value, which the user typed in and show it in a div which we position on top of the img.
-
+  // Show/Hide resize grabber and broder of memeText
+  const resizeGrabberHandler = () => showMemeGrabber ? setShowMemeGrabber(false) : setShowMemeGrabber(true);
   return (
-    <div className={`meme-container ${context.theme === 'light' ? 'box-shadow-light-lg' : 'box-shadow-dark-lg'}`}>
+    <div
+      className={`meme-container ${
+        context.theme === "light" ? "box-shadow-light-lg" : "box-shadow-dark-lg"
+      }`}
+      onClick={(e) => !e.target.matches('.meme-text') && setShowMemeGrabber(false)}
+    >
       <div className="form">
         <div className="meme-input-container">
           <input
@@ -49,29 +73,52 @@ export default function MemeGenerator() {
             name="topText"
             onChange={handleChange}
             type="text"
-            className={context.theme === 'light' ? 'box-shadow-light-small' : 'box-shadow-dark-small'}
+            className={
+              context.theme === "light"
+                ? "box-shadow-light-small"
+                : "box-shadow-dark-small"
+            }
             placeholder="Shut up"
+            maxLength={50}
           ></input>
           <input
             value={meme.bottomText}
             name="bottomText"
             onChange={handleChange}
             type="text"
-            className={context.theme === 'light' ? 'box-shadow-light-small' : 'box-shadow-dark-small'}
+            className={
+              context.theme === "light"
+                ? "box-shadow-light-small"
+                : "box-shadow-dark-small"
+            }
             placeholder="and take my money"
+            maxLength={50}
           ></input>
         </div>
-        <button onClick={getMemeImage} className={context.theme === 'light' ? 'box-shadow-light-small' : 'box-shadow-dark-small'}>
+        <button
+          onClick={getMemeImage}
+          className={
+            context.theme === "light"
+              ? "box-shadow-light-small"
+              : "box-shadow-dark-small"
+          }
+        >
           Get a new meme <i className="fa-regular fa-image"></i>
         </button>
       </div>
-    
-      <div className={`meme-image-container ${context.theme === 'light' ? 'bg-light-container' : 'bg-dark-container'}`}>
-        <div className="meme-text text-top">{meme.topText}</div>
-        <div className="meme-text text-bottom">{meme.bottomText}</div>
-        <div>
+
+      <div
+        className={`meme-image-container ${
+          context.theme === "light" ? "bg-light-container" : "bg-dark-container"
+        }`}
+      >
+        <Draggable bounds="parent">
+          <div onClick={resizeGrabberHandler} className={`meme-text text-top ${showMemeGrabber && 'showGrabberHandler'}`}>{meme.topText}</div>
+        </Draggable>
+        <Draggable bounds="parent">
+          <div onClick={resizeGrabberHandler} className="meme-text text-bottom">{meme.bottomText}</div>
+        </Draggable>
           <img className="meme-img" src={meme.randomImage} alt="meme" />
-        </div>
       </div>
     </div>
   );
